07e2844f9ef02e24c6a6a95c9cf6bbd762d9d8b7
David Lara <david.lara.moreira@udla.edu.ec>
2023-06-19 16:20:32 -0500

    Segunda version sin comentar

--- tallerestructuras.c
+++ tallerestructuras.c
@@ -9,42 +9,24 @@ int main()
         char carrera[20];
         int edad;
         float promedio;
-    } a1;
+    } a1, a2, a3, a4, a5;
+    struct alumno alumnos[5];
+for (int i = 0; i < 5; i++)
+{
+    printf("Ingrese los datos del alumno %d :", i+1);
+    gets(a1.nombre);
+    fflush(stdin );
+    gets(a1.direccion);
+    fflush( stdin );
+    gets(a1.carrera);
+    fflush( stdin );
+    scanf("Ingrese el nombre:%d"a1.edad);
+    scanf("Ingrese el nombre:%.2f"a1.promedio);
+}
+for (int i = 0; i < 5; i++)
+{
+    printf("Los datos de alumno %d son: %s,%s,%s,%d,%.2f", i+1,a1.nombre, a1.direccion, a1.carrera, a1.edad, a1.promedio);
+}
 
-    puts("Ingrese los datos del alumno 1 :");
-    gets(a1.nombre);
-    gets(a1.direccion);
-    gets(a1.carrera);
-    gets(a1.edad);
-    gets(a1.promedio);
-    printf("%s, %s, %s, %d, %.2f", a1.nombre, a1.direccion, a1.carrera, a1.edad, a1.promedio);
-    puts("Ingrese los datos del alumno 2 :");
-    gets(a1.nombre);
-    gets(a1.direccion);
-    gets(a1.carrera);
-    gets(a1.edad);
-    gets(a1.promedio);
-    printf("%s, %s, %s, %d, %.2f", a1.nombre, a1.direccion, a1.carrera, a1.edad, a1.promedio);
-    puts("Ingrese los datos del alumno 3 :");
-    gets(a1.nombre);
-    gets(a1.direccion);
-    gets(a1.carrera);
-    gets(a1.edad);
-    gets(a1.promedio);
-    printf("%s, %s, %s, %d, %.2f", a1.nombre, a1.direccion, a1.carrera, a1.edad, a1.promedio);
-    puts("Ingrese los datos del alumno 4 :");
-    gets(a1.nombre);
-    gets(a1.direccion);
-    gets(a1.carrera);
-    gets(a1.edad);
-    gets(a1.promedio);
-    printf("%s, %s, %s, %d, %.2f", a1.nombre, a1.direccion, a1.carrera, a1.edad, a1.promedio);
-    puts("Ingrese los datos del alumno 5 :");
-    gets(a1.nombre);
-    gets(a1.direccion);
-    gets(a1.carrera);
-    gets(a1.edad);
-    gets(a1.promedio);
-    printf("%s, %s, %s, %d, %.2f", a1.nombre, a1.direccion, a1.carrera, a1.edad, a1.promedio);
     return 0;
 }
